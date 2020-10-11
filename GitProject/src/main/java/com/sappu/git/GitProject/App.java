c59459b74739e02e71f32f8eb2af052637e849d8
sappuqa <ejagiri@gmail.com>
2020-10-11 13:56:40 -0500

    First Commit

---
+++ GitProject/src/main/java/com/sappu/git/GitProject/App.java
@@ -0,0 +1,13 @@
+package com.sappu.git.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GitProject/src/test/java/com/Selenium/DataDriver/DataDriven.java
@@ -0,0 +1,47 @@
+package com.Selenium.DataDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataDriven {
+	@DataProvider (name = "FB-login")
+	public Object [][] credentials(){
+		return new Object[][] {{"abc@gmail.com","123@Test"},{"def@gmail.com","456@test"}};
+	}
+		WebDriver driver;
+
+	public static String testURL = "https://www.facebook.com";
+
+	@BeforeMethod
+		public void setUp() {
+			System.setProperty("webdriver.chrome.driver", "C:\\seleniumdriver\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+		}
+	@Test(dataProvider = "FB-login")
+	public void testFBcredentials(String unames, String password) {
+		driver.get("https://www.facebook.com");
+		
+		//driver.findElement(By.xpath("//*[@id='email']")).sendKeys(unames);
+		WebElement emailEl = driver.findElement(By.xpath("//*[@id='email']"));
+		emailEl.sendKeys(unames);
+		
+		driver.findElement(By.cssSelector("#pass")).sendKeys(password);
+		//driver.findElement(By.cssSelector("#u_0_2")).click();
+		WebElement submitEl = driver.findElement(By.cssSelector("#u_0_2"));
+		submitEl.click();
+		
+		String actualresult = driver.getTitle();
+		Assert.assertTrue(actualresult.contains("Log into Facebook"));
+		//Assert.assertEquals(actualresult, "Log into Facebook|Facebook");
+		
+		
+	}
+
+}
