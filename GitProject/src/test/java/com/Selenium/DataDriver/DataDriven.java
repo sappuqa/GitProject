e6a970a79e9cb5dc624439d33d8f14dc374caef6
sappuqa <ejagiri@gmail.com>
2020-10-12 11:47:43 -0500

    yyy

--- GitProject/src/test/java/com/Selenium/DataDriver/DataDriven.java
+++ GitProject/src/test/java/com/Selenium/DataDriver/DataDriven.java
@@ -18,7 +18,7 @@ public class DataDriven {
 
 	public static String testURL = "https://www.facebook.com";
 // test
-	// sprint3 added new things
+	// sprint3 added new things  vv
 	@BeforeMethod
 		public void setUp() {
 			System.setProperty("webdriver.chrome.driver", "C:\\seleniumdriver\\chromedriver_win32\\chromedriver.exe");
